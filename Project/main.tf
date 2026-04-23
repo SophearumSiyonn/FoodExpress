ed02d21672d4319c73ce0d89036b5d53d0fc31d6
Sophearum Siyonn <2023420siyonn@aupp.edu.kh>
2026-04-23 13:55:10 +0700

    main.tf commit

---
+++ Project/main.tf
@@ -0,0 +1,207 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "repo_url" {
+  type        = string
+  description = "GitHub repository URL to clone on EC2"
+}
+
+variable "image_name" {
+  type        = string
+  description = "Docker image name to build locally on EC2"
+  default     = "foodexpress-app:latest"
+}
+
+resource "aws_key_pair" "labsuser" {
+  key_name   = "labsuser"
+  public_key = file("${path.module}/labsuser.pub")
+}
+
+resource "aws_default_vpc" "default" {}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "foodexpress-alb-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  name        = "foodexpress-ec2-sg"
+  description = "Allow SSH and app traffic from ALB"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 5000
+    to_port         = 5000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_launch_template" "app_lt" {
+  name_prefix   = "foodexpress-launch-template-"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.labsuser.key_name
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.app_sg.id]
+  }
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    set -e
+
+    apt-get update -y
+    apt-get install -y docker.io git
+
+    systemctl start docker
+    systemctl enable docker
+
+    usermod -aG docker ubuntu
+
+    rm -rf /home/ubuntu/app
+    git clone ${var.repo_url} /home/ubuntu/app
+    cd /home/ubuntu/app
+
+    docker build -t ${var.image_name} .
+    docker rm -f deploy-app || true
+    docker run -d --name deploy-app -p 5000:5000 ${var.image_name}
+  EOF
+  )
+}
+
+resource "aws_lb" "app_lb" {
+  name               = "foodexpress-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "foodexpress-app-tg"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+
+  health_check {
+    path                = "/health"
+    port                = "5000"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+  }
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+resource "aws_autoscaling_group" "app_asg" {
+  name                = "foodexpress-asg"
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  target_group_arns   = [aws_lb_target_group.app_tg.arn]
+  min_size            = 1
+  desired_capacity    = 1
+  max_size            = 3
+
+  launch_template {
+    id      = aws_launch_template.app_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "FoodExpress-App-Instance"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_policy" "cpu_policy" {
+  name                   = "foodexpress-cpu-policy"
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name = aws_autoscaling_group.app_asg.name
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 50.0
+  }
+}
+
+output "website_url" {
+  value = "http://${aws_lb.app_lb.dns_name}"
+}
